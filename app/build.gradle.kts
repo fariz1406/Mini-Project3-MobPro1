9cd8663fe0ac78bfebfa3a00d00b504232572172
Fariz Rahman Nst <farizrahmannst14@gmail.com>
2025-06-13 17:20:28 +0700

    Memproses data string sebagai JSON

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,13 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.scalars)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.moshi.core)
+    implementation(libs.moshi.kotlin)
+    implementation(libs.moshi.converter)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
